f550dceb387c63ac350e5394c44578d2a3c48e3a
Chris Healer <chris@eyelash.ai>
2023-03-22 20:10:23 -0400

    tweaking doxygen setup

--- documentation/env.sh
+++ documentation/env.sh
@@ -5,7 +5,10 @@ export PROJECT_BRIEF='Documentation for the C# Library'
 export PROJECT_LOGO='images/defaultIcon.png'
 export PROJECT_REPO='https://github.com/PixoVR/com.pixovr.apexunitysdk'
 export PROJECT_URL='ApexSDK-Unity'
-export PROJECT_URL='../../../../Unity/com.pixovr.apexunitysdk/documentation/html/index.html'
+export DEV_PROJECT_URL='../../../../Unity/com.pixovr.apexunitysdk/documentation/html/index.html'
+
+export DOXYGEN_FILTER='./scripts/unity_filter.py'
+export DOXYGEN_INPUT='"../pages"\n"../../Runtime"\n"../../Lib"\n"../../Samples"'
 
 export APEX_SERVER_URL='https://apex.pixovr.com'
 export MAIN_PAGE='../pages/main.md'
