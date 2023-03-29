2e6d21ca3eca697f97708b4c583a5973b72261a9
Chris Healer <chris@eyelash.ai>
2023-03-29 14:17:12 -0400

    added PROJECT_STATUS

--- documentation/env.sh
+++ documentation/env.sh
@@ -4,6 +4,7 @@ export PROJECT_NAME='ApexSDK - Unity'
 export PROJECT_VERSION=`cd ../; git tag | tail -n 1`
 export PROJECT_BRIEF='Documentation for the Unity C# Library'
 #export PROJECT_BRIEF='Documentation for the C# Library'
+export PROJECT_STATUS='active'
 export PROJECT_LOGO='docs-doxygen/doxygen-custom/defaultIcon.png'
 export PROJECT_REPO='https://github.com/PixoVR/com.pixovr.apexunitysdk'
 export PROJECT_URL='/ApexSDK-Unity'
