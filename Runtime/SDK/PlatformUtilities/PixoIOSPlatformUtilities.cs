4c14408db5b0ab2d9517c356bab4eba6d6c47b2f
Malcolm Gruber <malcolm.gruber@pixovr.com>
2025-06-04 14:01:56 -0400

    Add additional logging to the iOS and Mac OSX utility files.

--- Runtime/SDK/PlatformUtilities/PixoIOSPlatformUtilities.cs
+++ Runtime/SDK/PlatformUtilities/PixoIOSPlatformUtilities.cs
@@ -10,11 +10,13 @@ namespace PixoVR.Apex
     {
         public PixoIOSPlatformUtilities() : base()
         {
+            UDebug.Log($"{GetType().Name}");
 
         }
 
         public override bool OpenURL(string url)
         {
+            UDebug.Log($"{GetType().Name}::OpenURL");
             if (string.IsNullOrEmpty(url))
             {
                 UDebug.Log("Url is empty or null.");
@@ -27,6 +29,7 @@ namespace PixoVR.Apex
 
         public override bool OpenApplication(string applicationPath, string[] argumentKeys, string[] argumentValues)
         {
+            UDebug.Log($"{GetType().Name}::OpenApplication");
             if (!string.IsNullOrEmpty(applicationPath))
             {
                 UDebug.Log("Application is empty.");
