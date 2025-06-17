76997d47de9957ffa149771b0045117b9eb3aa2a
Malcolm Gruber <malcolm.gruber@pixovr.com>
2025-06-17 00:59:33 -0400

    Updating the package version.

--- Runtime/SDK/ApexUtils.cs
+++ Runtime/SDK/ApexUtils.cs
@@ -100,7 +100,7 @@ namespace PixoVR.Apex.Utils
             return stringOfJson.toJObject();
         }
 
-        public static string SDKVersion => "1.6.5";
+        public static string SDKVersion => "1.6.6";
     }
 }
 
