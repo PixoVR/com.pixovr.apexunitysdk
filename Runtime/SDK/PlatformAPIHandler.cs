09fd688d698b246dbc1140576072563f02dc08ae
Malcolm Gruber <malcolm.gruber@pixovr.com>
2025-10-20 16:27:52 -0400

    Added more logging and made sure all access checks pass through the CheckModuleAccess function.

--- Runtime/SDK/PlatformAPIHandler.cs
+++ Runtime/SDK/PlatformAPIHandler.cs
@@ -429,6 +429,7 @@ namespace PixoVR.Apex
             handlingClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
 
             string optionalParameters = "";
+            Debug.Log($"Checking for a serial number: {serialNumber}");
             if (!string.IsNullOrEmpty(serialNumber))
             {
                 optionalParameters = "?serial=" + serialNumber;
