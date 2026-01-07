f03b5fbb53171f10e378a354e0b80ecdaa15c6b2
Joe Reeder <joseph.reeder@pixovr.com>
2026-01-07 15:59:53 -0500

    PLATFORM-1926 (Dev): fixed issue where a device would be offline but showing the last users name

--- Runtime/SDK/ApexTypes.cs
+++ Runtime/SDK/ApexTypes.cs
@@ -782,21 +782,22 @@ namespace PixoVR.Apex
         public void RefreshDisplayFields()
         {
             // Display name
-            if (user != null)
+            if (!online)
             {
-                currentUserDisplay = user.fullName;
+                currentUserDisplay = "Offline";
             }
             else
             {
-                if (!online)
-                    currentUserDisplay = "Offline";
-                else
-                    currentUserDisplay = "Available";
+                currentUserDisplay = "Available";
+                if (user != null)
+                {
+                    currentUserDisplay = user.fullName;
+                }
             }
 
+            if (online && currentApp != null)
+            {
 
-            if (online && currentApp != null) { 
-                
                 if (currentApp.id == PixoPlatformModuleIDs.HUBAPP_MODULE_ID)
                 {
                     currentModuleDisplay = "In Hub App";
@@ -891,6 +892,6 @@ namespace PixoVR.Apex
         }
     }
 
-    
+
 
 }
